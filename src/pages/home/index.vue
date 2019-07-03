3b8e1cf9f288bf9c153d714ade511159d6f6ec79
shawn wu <hqwuzhaoyi@qq.com>
2019-07-03 17:25:35 +0800

    结构搭建完

---
+++ src/pages/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <header class="g-header-container">
+            <div>
+                <home-header></home-header>
+            </div>
+        </header>
+        <div class="g-backtop-container"></div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+  import HomeHeader from './header';
+
+  export default {
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+    @import "~assets/scss/mixins";
+    .home {
+        overflow: hidden;
+        width: 100%;
+        height: 100%;
+        background-color: $bgc-theme;
+    }
+</style>
